95afb100719c5b49a9f4d3e4d5828c401dc7c1ea
mjsumpter <github@mjs.simplelogin.com>
2025-08-20 06:54:27 -0600

    Revert "fix: url_o to url_l"
    This reverts commit 42f74e880b6508fb5f74fdb3a53537860a3e40e4.

--- src/routes/api/photos/+server.ts
+++ src/routes/api/photos/+server.ts
@@ -7,7 +7,7 @@ export async function GET({ url }) {
 	const page = url.searchParams.get('page') || 1;
 	const tags = url.searchParams.get('tags') || '';
 
-	const endpoint = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${FLICKR_API_KEY}&user_id=${FLICKR_USER_ID}&format=json&nojsoncallback=1&extras=description,date_taken,url_m,url_l,tags&page=${page}&per_page=500`;
+	const endpoint = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${FLICKR_API_KEY}&user_id=${FLICKR_USER_ID}&format=json&nojsoncallback=1&extras=description,date_taken,url_m,url_o,tags&page=${page}&per_page=500`;
 	const tagFilter = tags ? `&tags=${tags}` : '';
 
 	const response = await fetch(endpoint + tagFilter);
